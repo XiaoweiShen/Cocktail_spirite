82facdab8a34cee9ad8b7631a91ed3f659369a46
Catmhhw <catherine.mhh.wu@gmail.com>
2023-02-27 18:23:29 -0500

    commented out lines 3 and 4 to allow app to run

--- db/migrate/20230223174858_add_foreign_keys_to_drink_ingredient.rb
+++ db/migrate/20230223174858_add_foreign_keys_to_drink_ingredient.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToDrinkIngredient < ActiveRecord::Migration[6.1]
   def change
-    add_column :drink_ingredients, :ingredient_id, :ingeger
-    add_column :drink_ingredients, :ingredient_name, :string
+    # add_column :drink_ingredients, :ingredient_id, :integer
+    # add_column :drink_ingredients, :ingredient_name, :string
   end
 end
