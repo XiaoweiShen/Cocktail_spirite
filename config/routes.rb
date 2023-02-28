f644c1d80480fa89dd1d9bbcddb4b2347f9ff9d2
Catmhhw <catherine.mhh.wu@gmail.com>
2023-02-27 22:49:02 -0500

    add react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
+  get 'app', to: 'spa#index'
+  get 'app/*path', to: 'spa#index'
+  # get 'homepage/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
