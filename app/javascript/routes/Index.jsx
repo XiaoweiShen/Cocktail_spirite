0bf8c67c9e778233775dcdf3ce95b83621d1fb21
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-03 06:20:31 -0500

    fix

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
-import DrinkMixer from "../components/DrinkMixer"
-import DrinkList from "../components/DrinkList"
-import Drinks from "../components/Drinks"
-
 
 export default (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Drinks" element={<Home />} />
-    </Routes>
-  </Router>
+	<Router>
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
+	</Router>
 );
