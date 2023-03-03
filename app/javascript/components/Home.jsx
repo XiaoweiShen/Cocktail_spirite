e51a312a080aa2bbffbcca5d89ec4b55770d155e
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-03 05:40:56 -0500

    add pages

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
 
 export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
---
+++ app/javascript/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+	<nav>
+		<div>
+			<h1>TESTER</h1>
+		</div>
+	</nav>
+}
