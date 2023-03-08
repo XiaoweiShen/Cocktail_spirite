76196ea54c43bc9d011096060650de7cfd467f4a
XiaoweiShen <snhiui@gmail.com>
2023-03-08 08:53:29 -0800

    api coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'glasses/index'
+  get 'glasses/show'
+  get 'drink_ingredients/index'
+  get 'drink_ingredients/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :drinks,only: [:index, :show, :edit, :update]
+  resources :ingredients,only: [:index, :show]
+  resources :glasses, only: [:index, :show]
+  resources :drink_ingredients, only: [:show]
 end
