214260f7777a33ff1b274ae201965c83e680f04e
XiaoweiShen <snhiui@gmail.com>
2023-03-08 10:58:18 -0800

    update!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'homepage#index'
-  match '*path' => 'homepage#index', via: :all
+ 
   resources :drinks,only: [:index, :show, :edit, :update]
   resources :ingredients,only: [:index, :show]
   resources :glasses, only: [:index, :show]
   resources :drink_ingredients, only: [:show]
+  root 'homepage#index'
+  match '*path' => 'homepage#index', via: :all
 end
