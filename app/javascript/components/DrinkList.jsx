ea8d5ce752ec589f5360c61ab43fd3208c99475f
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-09 04:37:59 -0500

    add semi colons

--- app/javascript/components/DrinkList.jsx
+++ app/javascript/components/DrinkList.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios"
-import { useEffect } from "react";
 
 export default () => {
 	const [drinks, setDrinks] = useState([]);
 	useEffect(() => {
 		axios.get("/api/drinks").then((res) => {
 			setDrinks(res.data)
-		})
+		});
 	}, []);
 
 	const drinkList = drinks.map(drink => {
@@ -18,12 +17,12 @@ export default () => {
 				<img src={drink.image}></img>
 			</div>
 		)
-	})
+	});
 
 	return (
 		<div>
 			<h1>drinks</h1>
 			{drinkList}
 		</div>
-	)
-}
+	);
+};
